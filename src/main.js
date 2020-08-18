659e8ccd952965862aad98448aea5b70bbf323e6
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-08-18 18:55:42 -0300

    Dinamic to-do tasks visualization, move variables from global scope to local scope on createItem()

--- src/main.js
+++ src/main.js
@@ -1,45 +1,47 @@
 const todoItems = [];
 const doneItems = [];
 
-// Create dynamic HTML elements
-const li = document.createElement("li");
-const input = document.createElement("input");
-const span = document.createElement("span");
-const button = document.createElement("button");
-
-// Get static HTML elements
-const keyInput = document.getElementById("add-item");
-const ulTodoItems = document.getElementById("todo-items");
-const ulDoneItems = document.getElementById("done-items");
+// Get HTML elements
+const keyInput = document.getElementById("add-task");
+const ulTodoItems = document.getElementById("todo-tasks");
+const ulDoneItems = document.getElementById("done-tasks");
 
 keyInput.addEventListener("keypress", (event) => {
   const keyValue = event.keyCode;
 
   if (keyValue === 13) {
     todoItems.push(keyInput.value);
-    createItem();
+    createItem(todoItems.slice(-1), todoItems.length - 1);
   }
 });
 
-const createItem = () => {
+const createItem = (item, index) => {
+  // Create HTML elements
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+
   // Add li item to ul
   ulTodoItems.append(li);
 
   // The next lines append the elements to li and set their attributes
   // Input
   li.append(input);
-  const itemIdNumber = todoItems.length;
   input.setAttribute("type", "checkbox");
-  input.setAttribute("name", "item");
-  input.setAttribute("id", itemIdNumber);
+  input.setAttribute("name", "check-task");
+  input.setAttribute("id", index);
 
   // Span
   li.append(span);
-  span.innerHTML = todoItems[todoItems.length - 1];
+  span.setAttribute("class", "task");
+  span.innerHTML = item;
 
   // Button
   li.append(button);
   button.setAttribute("class", "delete-button");
 };
+
+const moveTaskToDone = () => {};
 
 const deleteItem = () => {};
